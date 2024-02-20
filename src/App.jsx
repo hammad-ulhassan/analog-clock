1bb35bf5bae8c107a9f6eda0367cbff7feaebd1d
Hammad Hassan <hammad.hassan.murtaza@gmail.com>
2024-02-20 21:20:26 +0500

    somewhat

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@ import "./App.css";
 import minuteMarks from "./assets/minuteMarks.svg";
 import fiveMinuteMarks from "./assets/fiveMinuteMarks.svg";
 import secondHand from "./assets/secondHand.svg";
-// import minuteHand from "./assets/minuteHand.svg";
-// import hourHand from "./assets/hourHand.svg";
+import minuteHand from "./assets/minuteHand.svg";
+import hourHand from "./assets/hourHand.svg";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+
+  const secondHandRef = useRef( null )
+  const minuteHandRef = useRef( null )
+  const hourHandRef = useRef( null )
+  const [ ticker, setTicker ] = useState( 0 )
+
+  // useEffect(() => {
+  //   secondHandRef.current.style.transform="rotate("+ 240 +"deg)"
+  // }, [])
+
+  useEffect(() => {
+    const tickInterval = setInterval(() => {
+      setTicker(old => old + 1 )
+    }, 1000)
+    const date = new Date();
+    const time = {
+      second: date.getSeconds(),
+      minute: date.getMinutes(),
+      hour: date.getHours()
+    };
+    secondHandRef.current.style.transform="rotate("+ 240+( time.second * 6 ) +"deg)"; //240 since svg of second hand os at 240 degree from 12:00
+    minuteHandRef.current.style.transform="rotate("+ 48.5+( time.minute * 6 ) +"deg)"
+    hourHandRef.current.style.transform="rotate("+ ( time.minute/2 + ( time.hour * 30 ) ) +"deg)"
+    return () => clearInterval( tickInterval )
+  }, [ ticker ])
+
   return (
     <div
       style={{
@@ -14,16 +41,12 @@ function App() {
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        outline: "solid",
-        border: "1px solid red"
       }}
     >
       <img
         style={{
           height: "30%",
           position: "absolute",
-          outline: "solid",
-          border: "1px solid red"
         }}
         src={minuteMarks}
       />
@@ -32,16 +55,15 @@ function App() {
         style={{
           height: "30%",
           position: "absolute",
-          outline: "solid",
-          border: "1px solid red"
         }}
         src={fiveMinuteMarks}
       />
 
-      <img
-        className="second"
-        src={secondHand}
-      />
+      <img ref={secondHandRef} className="second" src={secondHand} />
+
+      <img ref={minuteHandRef} className="minute" src={minuteHand} />
+
+      <img ref={hourHandRef} className="hour" src={hourHand} />
     </div>
   );
 }
