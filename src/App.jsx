fc62cc2f1e6a8f4b9967f76f6076d14a95608fd7
Hammad Hassan <hammad.hassan.murtaza@gmail.com>
2024-02-20 17:35:39 +0500

    hands placement

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
-import './App.css'
-import secondHand from './assets/secondHand.svg'
-import minuteHand from './assets/minuteHand.svg'
-import hourHand from './assets/hourHand.svg'
+import "./App.css";
+import minuteMarks from "./assets/minuteMarks.svg";
+import fiveMinuteMarks from "./assets/fiveMinuteMarks.svg";
+import secondHand from "./assets/secondHand.svg";
+import minuteHand from "./assets/minuteHand.svg";
+import hourHand from "./assets/hourHand.svg";
 
 function App() {
-
   return (
-      <div style={ {
+    <div
+      style={{
         display: "flex",
-      } }>
-          <img className="second" src={secondHand} style={{ width: "100%" }}/>
-          <img className="minute" src={minuteHand} style={{ width: "100%" }}/>
-          <img className="minute" src={hourHand} style={{ width: "100%" }}/>
-      </div>
-  )
+        height: "100%",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        style={{
+          height: "30%",
+          position: "absolute",
+        }}
+        src={minuteMarks}
+      />
+
+      <img
+        style={{
+          height: "30%",
+          position: "absolute",
+        }}
+        src={fiveMinuteMarks}
+      />
+
+      <img
+        style={{
+          position: "absolute",
+          transformOrigin: "0% 0%",
+        }}
+        src={secondHand}
+      />
+
+      <img
+        style={{
+          position: "absolute",
+          transformOrigin: "0% 0%",
+        }}
+        src={minuteHand}
+      />
+
+      <img
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transformOrigin: "0% 0%",
+        }}
+        src={hourHand}
+      ></img>
+    </div>
+  );
 }
 
-export default App
+export default App;
